5867f22065d1ad8f57bdb37229c9ee793af7165c
hjg17 <herbertjgomez@gmail.com>
2026-08-18 15:06:18 +0100

    Refactor Dockerfile to use specific Alpine version, add health check and API endpoint in main.go, implement security checks in GitHub Actions, and create go.mod file.

--- app/main.go
+++ app/main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"strings"
 )
+// Demo API key
+const hardCodedKey = "AIzaEXAMPLED99NlL8x9A9T3tJ9h118Zc1gC1DK"
 
 func main() {
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+	// Health check endpoint
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+
+	// API endpoint that requires an API key for access
+	http.HandleFunc("/api/v1/data", func(w http.ResponseWriter, r *http.Request) {
+		expected := os.Getenv("API_SECRET")
+		if expected == "" {
+			expected = hardCodedKey
+		}
+
+		// Check for the API key in the Authorization header
+		token := r.Header.Get("Authorization")
+
+		// Ideally, we would also check for empty tokens and deny authorization, but we will allow it to be empty for this demo
+		if token != expected {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Access granted")
+	})
+
 	http.ListenAndServe(":8080", nil)
 }
